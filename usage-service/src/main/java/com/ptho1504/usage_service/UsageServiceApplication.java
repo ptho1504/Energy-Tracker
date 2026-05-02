e09514a602adeca26f6186d5db435e2c316d9d29
ptho1504 <phuoctho150420@gmail.com>
2026-05-03 01:22:18 +0700

    feat(usage): Add InfluxDB and Consumer for Usage Service

---
+++ usage-service/src/main/java/com/ptho1504/usage_service/UsageServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ptho1504.usage_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class UsageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsageServiceApplication.class, args);
+	}
+
+}

154c87a1e341c3d8c706873754c8dacdd62dad45
ptho1504 <phuoctho150420@gmail.com>
2026-05-03 10:21:50 +0700

    feat(gateway): Add circuit breaker

--- api-gateway/src/main/java/com/ptho1504/api_gateway/route/UserServiceRoutes.java
+++ api-gateway/src/main/java/com/ptho1504/api_gateway/route/UserServiceRoutes.java
@@ -1,12 +1,16 @@
 package com.ptho1504.api_gateway.route;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.function.RequestPredicate;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
@@ -22,6 +26,19 @@ public class UserServiceRoutes {
         return route("user-service")
                 .route(RequestPredicates.path("/api/v1/user/**"), http())
                 .before(uri("http://localhost:8080"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker(
+                        "userServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")
+                ))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> userFallbackRoute() {
+        return route("fallbackRoute")
+                .route(RequestPredicates.path("/fallbackRoute"),
+                        request-> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("User service is down"))
                 .build();
     }
 }
